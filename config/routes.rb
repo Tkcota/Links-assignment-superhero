ac659346a6fbf1c7109115811cf3de4a20d30541
Tyler K <tkcota@gmail.com>
2022-06-07 14:39:10 -0600

    routeupdate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/superheroes', to:'superheroes#index'
-    get '/superheroes', to: 'dishes#show' 
+    get '/superheroes', to: 'superheroes#show' 
     post '/superheroes', to: 'superheroes#create' 
     put '/superheroes', to: 'superheroes#update' 
     delete '/superheroes', to: 'superheroes#destroy'
