c7808e25f53ec6ebbde0440d712ddd7f78c4b6c9
Colby Patterson <cpatt@byui.edu>
2022-06-07 17:20:52 -0600

    Added set_superhero method to controller

--- app/controllers/api/superheroes_controller.rb
+++ app/controllers/api/superheroes_controller.rb
@@ -1,4 +1,5 @@
 class Api::SuperheroesController < ApplicationController
+before_action :set_superhero, only:[:show, :update, :destroy]
 
 def index 
   superheroes=Superhero.all
@@ -6,39 +7,41 @@ def index
 end
 
 def show
-  @superhero = Superhero.find(params[:id])
   render json: @superhero
 end
 
 def create 
-  @superhero = Superhero.new(superhero_params)
-  if(@superhero.save)
-    render json:@superhero
+  superhero = Superhero.new(superhero_params)
+  if(@uperhero.save)
+    render json: superhero
   else
    # render json:{ @superhero.errors.full_messages,status:422}
  end
 end
 
 def update
-  @superheroes = Superhero.find(params[:id])
-  if @superheroes.update(superhero_params)
-    render json: @superheroes
+  if @superhero.update(superhero_params)
+    render json: @superhero
   else
   end
 end
 
 def destroy
   # render json: @superhero.destroy
- @superheroes = Superhero.find(params[:id])
- if @superheroes.destroy
-  render json: @superheroes
+ if @superhero.destroy
+  render json: @superhero
  else
  end
 end
   
 
 
-private 
+private
+
+  def set_superhero
+    @superhero = Superhero.find(params[:id])
+  end
+
   def superhero_params 
     params.require(:Superhero).permit(:name,:power)
   end
